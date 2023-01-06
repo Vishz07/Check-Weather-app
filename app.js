adaf68d613024960d2f4ea90ec8d2e5f673b79a3
Vishz07 <anandnairaok@gmail.com>
2023-01-06 07:56:48 +0530

    apps-update-
    Clearing results before every search

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var url = "https://api.weatherapi.com/v1/current.json?key=fa6430e826fd4c20865941
 var temp = document.querySelector("#temp");
 var condition = document.querySelector("#condition");
 var wind = document.querySelector("#wind");
-var humidity = document.querySelector("#region");
+var region = document.querySelector("#region");
 var kmph = document.querySelector("#kmph");
 
 
@@ -16,13 +16,19 @@ return url + "=" + inputtext + "&aqi=no"
 }
 
 function weatherChecker(){
-  
+
+  temp.innerText = " ";
+  condition.innerText = " ";
+  wind.innerText = " ";
+  region.innerText = " ";
+
   inputtext = input.value
+
   fetch(apiurl(inputtext)).then(Response => Response.json()).then(json => {
     temp.innerText+= " " +json.current.temp_c;
     condition.innerText+= " "+json.current.condition.text;
     wind.innerText+= " "+ json.current.wind_kph;
-    humidity.innerText+= " "+ json.location.region;
+    region.innerText+= " "+ json.location.region;
   })
   
 }
